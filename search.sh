ba9e518534a11736809b2d46a8a8c67cd17f12e5
Ernesto Celis <celisdelafuente@gmail.com>
2018-02-25 15:46:44 -0600

    wip selection

--- search.sh
+++ search.sh
@@ -1,32 +1,55 @@
 #!/bin/bash
 
 ## Author: Hernando Ramos <aku@esblender.org>
+## Menu from
+## https://serverfault.com/questions/144939/multi-select-menu-in-bash-script
+## 2013-05-10 - Dennis Williamson
 
+OS=$(uname)
+
+if [ ${OS} = "Darwin" ];
+then
+  OPENCMD="open"
+else
+  OPENCMD="xdg-open"
+fi
 
 . ./settings.conf
 
-echo "
+choice () {
+  local choice=$1
+  if [[ ${opts[choice]} ]]
+  then
+    opts[choice]=
+  else
+    opts[choice]=+
+  fi
+}
 
-/// /// /// /// /// /// /// /// /// /// /// /// /// /// /// ///
 
-╦ ╦╔═╗╦═╗╔╗╔╔═╗╔╗╔╔╦╗╔═╗
-╠═╣║╣ ╠╦╝║║║╠═╣║║║ ║║║ ║
-╩ ╩╚═╝╩╚═╝╚╝╩ ╩╝╚╝═╩╝╚═╝
-╔═╗╔═╗╔═╗╦═╗╔═╗╦ ╦
-╚═╗║╣ ╠═╣╠╦╝║  ╠═╣
-╚═╝╚═╝╩ ╩╩╚═╚═╝╩ ╩
-╔═╗╔╗╔╔═╗╦╔╗╔╔═╗
-║╣ ║║║║ ╦║║║║║╣
-╚═╝╝╚╝╚═╝╩╝╚╝╚═╝
+akubanner () {
+  echo "
+  /// /// /// /// /// /// /// /// /// /// /// /// /// /// /// ///
 
-/// /// /// /// /// /// /// /// /// /// /// /// /// /// /// ///"
+  ╦ ╦╔═╗╦═╗╔╗╔╔═╗╔╗╔╔╦╗╔═╗
+  ╠═╣║╣ ╠╦╝║║║╠═╣║║║ ║║║ ║
+  ╩ ╩╚═╝╩╚═╝╚╝╩ ╩╝╚╝═╩╝╚═╝
+  ╔═╗╔═╗╔═╗╦═╗╔═╗╦ ╦
+  ╚═╗║╣ ╠═╣╠╦╝║  ╠═╣
+  ╚═╝╚═╝╩ ╩╩╚═╚═╝╩ ╩
+  ╔═╗╔╗╔╔═╗╦╔╗╔╔═╗
+  ║╣ ║║║║ ╦║║║║║╣
+  ╚═╝╝╚╝╚═╝╩╝╚╝╚═╝
 
+  /// /// /// /// /// /// /// /// /// /// /// /// /// /// /// ///"
+}
+
+akubanner
 echo "What are you looking for?"
 
 read varsearch
 
 echo -e ">   >>   >>>   >>>>   >>>>>   >>>>>>   searching...\n"
-
 #       Extraer lineas de texto con el string buscado. La segunda opción funciona para coincidencias parciales en palabras
 #grep -rnwi /Users/nando/Desktop/INDEXADO/ -e ""
 for vol in $REL_PATHS; do
@@ -34,34 +57,29 @@ for vol in $REL_PATHS; do
   grepout=$(grep -ri ${idxfile} -e "${varsearch}")
   echo -e $grepout
 done
-
 echo -e "/// /// /// /// /// /// /// /// /// /// /// /// /// /// /// ///"
-
 echo -e "The End"
-
 echo -e "/// /// /// /// /// /// /// /// /// /// /// /// /// /// /// ///\n"
 
-#
-# Hace tiempo trate de encontrara la manera de que en cada string que regresa la busqueda me pusiera un hyperlink a la carpeta correspondiente pero no e encontrado la manera. También quisiera poner algo que hiciera que haga menos verboso la salida de search.sh, pues ahora es mas a menos así:
-#
-# /Volumes/NANDO_SCRATCH/INDEXADO//Lista_archivos en capturas.txt:/Volumes/VIDEO/001 CAPTURAS/./201709/20170911 ENTREGA DE PATRULLAS EN LA GANADERA VICTORIA
-# /Volumes/NANDO_SCRATCH/INDEXADO//Lista_archivos en capturas.txt:/Volumes/VIDEO/001 CAPTURAS/./201709/20170911 ENTREGA DE PATRULLAS EN LA GANADERA VICTORIA/FINALES
-# /Volumes/NANDO_SCRATCH/INDEXADO//Lista_archivos en capturas.txt:/Volumes/VIDEO/001 CAPTURAS/./201709/20170911 ENTREGA DE PATRULLAS EN LA GANADERA VICTORIA/master
-# /Volumes/NANDO_SCRATCH/INDEXADO//Lista_archivos en capturas.txt:/Volumes/VIDEO/001 CAPTURAS/./201709/20170911 ENTREGA DE PATRULLAS EN LA GANADERA VICTORIA/GRAFICOS
-# /Volumes/NANDO_SCRATCH/INDEXADO//Lista_archivos en capturas.txt:/Volumes/VIDEO/001 CAPTURAS/./201709/20170911 ENTREGA DE PATRULLAS EN LA GANADERA VICTORIA/AUDIO
-# /Volumes/NANDO_SCRATCH/INDEXADO//Lista_archivos en capturas.txt:/Volumes/VIDEO/001 CAPTURAS/./201709/20170911 ENTREGA DE PATRULLAS EN LA GANADERA VICTORIA/MATERIAL
-# /Volumes/NANDO_SCRATCH/INDEXADO//Lista_archivos en capturas.txt:/Volumes/VIDEO/001 CAPTURAS/./201709/20170911 ENTREGA DE PATRULLAS EN LA GANADERA VICTORIA/MATERIAL/OSMO
-# /Volumes/NANDO_SCRATCH/INDEXADO//Lista_archivos en capturas.txt:/Volumes/VIDEO/001 CAPTURAS/./201709/20170911 ENTREGA DE PATRULLAS EN LA GANADERA VICTORIA/MATERIAL/100CANON
-# /Volumes/NANDO_SCRATCH/INDEXADO//Lista_archivos en capturas.txt:/Volumes/VIDEO/001 CAPTURAS/./201709/20170911 ENTREGA DE PATRULLAS EN LA GANADERA VICTORIA/MATERIAL/CAM 03
-# /Volumes/NANDO_SCRATCH/INDEXADO//Lista_archivos en capturas.txt:/Volumes/VIDEO/001 CAPTURAS/./201709/20170911 ENTREGA DE PATRULLAS EN LA GANADERA VICTORIA/MATERIAL/101CANON
-# /Volumes/NANDO_SCRATCH/INDEXADO//Lista_archivos en capturas.txt:/Volumes/VIDEO/001 CAPTURAS/./201709/20170911 ENTREGA DE PATRULLAS EN LA GANADERA VICTORIA/PROYECTOS
-# /Volumes/NANDO_SCRATCH/INDEXADO//Lista_archivos en capturas.txt:/Volumes/VIDEO/001 CAPTURAS/./201709/20170911 ENTREGA DE PATRULLAS EN LA GANADERA VICTORIA/PROYECTOS/PR
-# /Volumes/NANDO_SCRATCH/INDEXADO//Lista_archivos en capturas.txt:/Volumes/VIDEO/001 CAPTURAS/./201709/20170911 ENTREGA DE PATRULLAS EN LA GANADERA VICTORIA/PROYECTOS/AE
-# /Volumes/NANDO_SCRATCH/INDEXADO//Lista_archivos en capturas.txt:/Volumes/VIDEO/001 CAPTURAS/./201709/20170911 ENTREGA DE PATRULLAS EN LA GANADERA VICTORIA/PROYECTOS/AU
-# /Volumes/NANDO_SCRATCH/INDEXADO//Lista_archivos en capturas.txt:/Volumes/VIDEO/001 CAPTURAS/./201709/20170911 ENTREGA DE PATRULLAS EN LA GANADERA VICTORIA/DOCS
-# /Volumes/NANDO_SCRATCH/INDEXADO//Lista_archivos en capturas.txt:/Volumes/VIDEO/001 CAPTURAS/./201709/20170904 EVENTO UNIDOS POR REYNOSA/MATERIAL/PATRULLAS
-# /Volumes/NANDO_SCRATCH/INDEXADO//Lista_archivos_Osito.txt:/Volumes/NANDO_SCRATCH/DISCO OSITO/./2017/5 MAYO 2017/2017_05_07 TOMAS DE RECORRIDO DE PATRULLAS
-# /// /// /// /// /// /// /// /// /// /// /// /// /// /// /// ///
-# Fin de la busqueda
-# /// /// /// /// /// /// /// /// /// /// /// /// /// /// /// ///
-#
+while :
+do
+  clear
+  #options=("1 ${}")
+  select $opt in "${grepout[@]}"
+  do
+    case $opt in
+      "1 ${opts[1]}")
+        choice 1
+        break
+        ;;
+      "2 ${opts[2]}")
+        choice 2
+        break
+        ;;
+      "Done")
+        break 2
+        ;;
+      *) printf '%s\n' 'Invalid option';;
+    esac
+  done
+done
