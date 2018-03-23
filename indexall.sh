8d3a54170f75047dfa951d9980de05698ba7f739
Ernesto Celis <celisdelafuente@gmail.com>
2018-03-23 17:45:11 -0600

    Fix indexall.sh not indexing

--- indexall.sh
+++ indexall.sh
@@ -9,7 +9,7 @@ echo -e "Indexing directories...\n"
 
 for vol in $REL_PATHS; do
   echo ${BASE_PATH}${vol}
-  find ${BASE_PATH}${vol} -maxdepth 8 -type d > ${BASE_PATH}${vol}/index_${vol}.idx
+  find ${BASE_PATH}${vol} -maxdepth 8 -type f > ${BASE_PATH}${vol}/index_${vol}.idx
 done
 
 echo -e "\n>>>>>>>>  Ready!!! Directories fully indexed.  <<<<<<<<\n"
